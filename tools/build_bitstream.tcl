a43779e49263c51944ff8c91ebb02723d373334d
z4yx <z4yx@users.noreply.github.com>
2018-10-20 16:46:31 +0800

    build for two hardware revisions

--- tools/build_bitstream.tcl
+++ tools/build_bitstream.tcl
@@ -1,5 +1,11 @@
 update_compile_order -fileset sources_1
 
+set rev [lindex $argv 0]
+puts "Build for thinpad rev.$rev"
+set_property generic thinpad_rev=$rev [current_fileset]
+set_property is_enabled [expr {$rev eq 2 ? true : false}] [get_files {*pins_rev2.xdc}]
+set_property is_enabled [expr {$rev eq 3 ? true : false}] [get_files {*pins_rev3.xdc}]
+
 reset_run impl_1
 reset_run synth_1
 launch_runs -jobs 2 impl_1 -to_step write_bitstream
